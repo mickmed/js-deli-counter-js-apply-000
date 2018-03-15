096492a63f4a6e7615c78df6fd8e423562c8edbf
Michael Roth <mick2090@gmail.com>
2018-03-15 14:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${katzDeliLine.slice(-1)}. You are number ${katzDeliLine.length} in line.`;
+function takeANumber(katzDeliLine, name, ticket){
+  // katzDeliLine.push(name);
+  // return `Welcome, ${katzDeliLine.slice(-1)}. You are number ${katzDeliLine.length} in line.`;
   
 }
 
